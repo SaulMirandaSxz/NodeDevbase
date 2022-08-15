37d99d9eb611a3ade18554605ca5f5a6fcb0af4b
Saul Miranda <36282163+SaulMirandaSxz@users.noreply.github.com>
2022-08-15 14:35:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const port = 3000;
+const port =  process.env.PORT || 5000;
 
 //Idiomatic expression in express to route and respond to a client request
 app.get('/', (req, res) => {        //get requests to the root ("/") will route here
