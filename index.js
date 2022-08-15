cd052ddd4103abcd97d3881e666050b3720471a0
Saul Miranda <36282163+SaulMirandaSxz@users.noreply.github.com>
2022-08-15 15:50:48 -0500

    POST Method

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+
 const express = require('express');
 
 const app = express();
 
 const port =  process.env.PORT || 5000;
+
+app.arguments(express.json());
+
+app.post('/api/', (req, res) => {
+    const id = req.params.id;
+    const body = req.body;
+    
+});
+
+
+
+
+
 
 //Idiomatic expression in express to route and respond to a client request
 app.get('/', (req, res) => {        //get requests to the root ("/") will route here
@@ -13,19 +27,3 @@ app.get('/', (req, res) => {        //get requests to the root ("/") will route 
 app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
     console.log(`Now listening on port ${port}`); 
 });
-
-
-
-
-const base = require('airtable').base('appYCmV6o07hjijQn');
-
-var Airtable = require('airtable');
-Airtable.configure({
-    endpointUrl: 'https://api.airtable.com',
-    apiKey: 'key8EGyjgLsDAH3Qt'
-});
-var base = Airtable.base('appYCmV6o07hjijQn');
-
-console.log(base);
-
-console.log("Hello World");
