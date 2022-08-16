ce9c692fe2e177b40394a17805f83e2a45da7db8
Saul Miranda <36282163+SaulMirandaSxz@users.noreply.github.com>
2022-08-16 11:46:40 -0500

    POST method

--- index.js
+++ index.js
@@ -32,19 +32,6 @@ app.get('/api/', (req, res) => {
 
 
 
-
-
-//Idiomatic expression in express to route and respond to a client request
-app.get('/', (req, res) => {        //get requests to the root ("/") will route here
-    res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
-                                                        //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
-});
-
-app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
-    console.log(`Now listening on port ${port}`); 
-});
-
-
 axios.post("https://devbase-node.herokuapp.com/api/airtable", data).then(res => {
     console.log('Status Code : ${res.status}');
     conselog.log('Body : ${JSON.stringify(res.data)}');
