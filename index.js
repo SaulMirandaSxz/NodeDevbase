a9998e1c1b0eb0e9693831627a56388be1724db2
Saul Miranda <36282163+SaulMirandaSxz@users.noreply.github.com>
2022-08-16 12:17:53 -0500

    main

--- index.js
+++ index.js
@@ -5,36 +5,17 @@ const app = express();
 
 const port =  process.env.PORT || 5000;
 
-const axios = require('axios');
+const axios = require('axios').default;
 
 const data = JSON.stringify({
     name: "airtable",
 });
 
-const options = {
-    hostname: "localhost:5000",
-    path: '/api/airtable',
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    }
-};
-
-
-
-
-app.get('/api/', (req, res) => {
-    res.status(200).send({
-        message: 'Welcome to the Devbase API Service',
-    })
-});
-
-
 
 
 axios.post("https://devbase-node.herokuapp.com/api/airtable", data).then(res => {
-    console.log('Status Code : ${res.status}');
-    conselog.log('Body : ${JSON.stringify(res.data)}');
+    console.log(`Status Code : ${res.status}`);
+    console.log(`Body : ${JSON.stringify(res.data)}`);
 }).catch(err => {
     console.log(err);
 })
