b0562f9c9906c72629b13806f052dc856b082354
Saul Miranda <36282163+SaulMirandaSxz@users.noreply.github.com>
2022-08-16 23:38:01 -0500

    Server Fixes

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser')
 
 const app = express();
 
+const axios = require('axios').default;
+
 // Create appliation/json parser
 var JsonParser = bodyParser.json();
 
@@ -12,12 +14,7 @@ var UrlEncodedParser = bodyParser.urlencoded({ extended: false });
 
 const port =  process.env.PORT || 5000;
 
-const axios = require('axios').default;
 
-
-const data = JSON.stringify({
-    name: "airtable",
-});
 
 const expenses = [
     { name: "string", amount: 1500, file: "string.jpg" },
@@ -54,15 +51,6 @@ app.post('/api/expenses', JsonParser, function (req, res) {
 
 
 
-//Idiomatic expression in express to route and respond to a client request
-app.get('/', (req, res) => {        //get requests to the root ("/") will route here
-    res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
-                                                        //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
-});
-
-app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
-    console.log(`Now listening on port ${port}`); 
-});
 
 
 
